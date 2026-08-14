4bcf3d10e35c2be95a8a9511dccf2453007036e6
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2026-08-14 21:07:38 +0300

    development

---
+++ tools/vite-capacitor-serve-android.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source .env
+echo "Running Vite with arguments: $@"
+echo "If you tried to npm run dev, build, or preview, make sure to use '--'(double dash) before the arguments."
+cd apps/$1
+vite build ${@:2}
+npx cap sync
+JAVA_HOME=$GRADLE_JDK ANDROID_HOME=$ANDROID_HOME ANDROID_JDK=$ANDROID_JDK npx cap run android
