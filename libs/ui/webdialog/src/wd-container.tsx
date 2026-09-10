5088749c21214c484e91924908416a29444e41a1
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2026-09-10 23:52:27 +0300

    wd changes

--- libs/ui/webdialog/src/wd-container.tsx
+++ libs/ui/webdialog/src/wd-container.tsx
@@ -1,4 +1,8 @@
-import { NeolitComponent, type NeolitNode } from "@ubs-platform/neolit/core";
+import {
+  NeolitComponent,
+  state,
+  type NeolitNode,
+} from "@ubs-platform/neolit/core";
 import { WebdialogOverlayService } from "./wd-overlay.service";
 import type { WebDialogConfig } from "./wd-overlay.service";
 import { WebDialog } from "./_index";
@@ -12,6 +16,7 @@ type DialogStackItem = {
   dialogConfig: WebDialogConfig;
   dialogId: number;
   dialogDom: NeolitNode | NeolitNode[] | NeolitComponent | HTMLElement;
+  closed: boolean;
 };
 
 export class WebDialogContainer extends NeolitComponent {
@@ -31,14 +36,23 @@ export class WebDialogContainer extends NeolitComponent {
 
   addDialogToStack(dialog: WebDialogConfig) {
     const addMask = this.dialogStack.length === 0;
+    const showState = state(true);
+    showState.subscribe((a) => {
+      if (!a) {
+        this.closeDialog(dialog);
+      }
+    });
+
+    this.dialogStack = this.dialogStack.filter((item) => !item.closed);
     this.dialogStack.push({
       dialogConfig: dialog,
       dialogId: this.dialogStack.length,
+      closed: false,
       dialogDom: (
         <WebDialog
           displayHeader={true}
           title={dialog.title}
-          show={true}
+          show={showState}
           showMask={addMask}
         >
           {
@@ -56,30 +70,38 @@ export class WebDialogContainer extends NeolitComponent {
   closeDialog(dialog: WebDialogConfig, closeValue?: any) {
     const index = this.dialogStack.findIndex((d) => d.dialogConfig == dialog);
     if (index !== -1) {
-      this.dialogStack[index].dialogConfig.onClose?.(closeValue);
-      this.dialogStack = this.dialogStack.slice(0, index).concat(this.dialogStack.slice(index + 1));
+      const dialogItem = this.dialogStack[index];
+      dialogItem.closed = true;
+      dialogItem.dialogConfig.onClose?.(closeValue);
+      this.dialogStack = this.dialogStack
+        .slice(0, index)
+        .concat(this.dialogStack.slice(index + 1));
       this.rerender();
     }
   }
 
   render(): NeolitNode | NeolitNode[] | NeolitComponent | null {
-    return <>
-      {this.dialogStack.map((a) => a.dialogDom)}
-      <WebDialog
-        show={this.webDialogOverlayService.isLoading}
-        mode="modal"
-        displayHeader={false}
-        displayCloseButton={false}
-        dismissOnClickMask={false}
-        width="280px"
-      >
-        {[
-          <div className="flex flex-col items-center gap-3 p-2 text-center">
-            <span className="material-symbols-outlined animate-spin">progress_activity</span>
-            <p>{this.webDialogOverlayService.loadingMessage}</p>
-          </div>,
-        ]}
-      </WebDialog>
-    </>;
+    return (
+      <>
+        {this.dialogStack.map((a) => a.dialogDom)}
+        <WebDialog
+          show={this.webDialogOverlayService.isLoading}
+          mode="modal"
+          displayHeader={false}
+          displayCloseButton={false}
+          dismissOnClickMask={false}
+          width="280px"
+        >
+          {[
+            <div className="flex flex-col items-center gap-3 p-2 text-center">
+              <span className="material-symbols-outlined animate-spin">
+                progress_activity
+              </span>
+              <p>{this.webDialogOverlayService.loadingMessage}</p>
+            </div>,
+          ]}
+        </WebDialog>
+      </>
+    );
   }
 }
